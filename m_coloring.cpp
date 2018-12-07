b001dff9783a25dc39092f2f5046f227f75f414e
elufe <41236080+elufe@users.noreply.github.com>
2018-12-07 21:10:54 +0900

    매개변수 수정

--- m_coloring.cpp
+++ m_coloring.cpp
@@ -4,12 +4,12 @@
 
 using namespace std;
 
-void m_coloring(int, int, int, int**,int *);
-bool promising(int, int **, int *);
-
+void m_coloring(int);
+bool promising(int);
+int n, m, **w, *vcolor;
 
 int main() {
-	int n, m, **w, *vcolor;
+
 	cout << "n?";
 	cin >> n;
 	cout << "m?";
@@ -18,34 +18,34 @@ int main() {
 	for (int i = 0; i < n; i++)
 		w[i] = new int[n];
 
-	cout << "w?"<<endl;
+	cout << "w?" << endl;
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
 			cin >> w[i][j];
 	vcolor = new int[n];
 
 
-	m_coloring(-1, n, m, w, vcolor);
+	m_coloring(-1);
 }
 
-void m_coloring(int i, int n, int m,int **w,int *vcolor) {
+void m_coloring(int i) {
 	int color;
-	if (promising(i, w, vcolor))
+	if (promising(i))
 		if (i == n - 1) {
 			for (int j = 0; j < n; j++)
 				cout << vcolor[j] << " ";
-			cout << "|";
+			cout << "| ";
 		}
 		else
 			for (color = 1; color <= m; color++) {
-				vcolor[i+1] = color;
-				m_coloring(i + 1,n,m,w,vcolor);
+				vcolor[i + 1] = color;
+				m_coloring(i + 1);
 			}
 }
 
-bool promising(int i, int **w, int *vcolor) {
-	int j=0;
-	bool s=true;
+bool promising(int i) {
+	int j = 0;
+	bool s = true;
 	while (j < i && s) {
 		if (w[i][j] && vcolor[i] == vcolor[j])
 			s = false;
--- sum_of_subset.cpp
+++ sum_of_subset.cpp
@@ -4,13 +4,24 @@
 
 using namespace std;
 
-void sum_of_subset(int, int, int, int*, bool*, int);
-bool promising(int i, int weight, int total, int *w, int W);
+void sum_of_subset(int, int, int);
+bool promising(int i, int weight, int total);
+int *w, W;
+bool *include;
+int compare(const void* a, const void* b) {
+	const int* x = (int*)a;
+	const int* y = (int*)b;
 
+	if (*x > *y)
+		return 1;
+	else if (*x < *y)
+		return -1;
+
+	return 0;
+}
 
 int main() {
-	int n, *w, total=0,W;
-	bool  *include;
+	int n, total = 0;
 	cout << "n?";
 	cin >> n;
 	cout << "W?";
@@ -24,17 +35,18 @@ int main() {
 		cin >> w[i];
 		total += w[i];
 	}
-
-	sum_of_subset(-1, 0, total, w, include,W);
+	qsort(w, n, sizeof(int), compare);
+	cout << w[0];
+	sum_of_subset(-1, 0, total);
 
 }
 
-void sum_of_subset(int i, int weight, int total, int *w, bool *include, int	W) {
+void sum_of_subset(int i, int weight, int total) {
 	int cnt = 0;
-	if (promising(i,weight,total,w,W)) {
+	if (promising(i, weight, total)) {
 		if (weight == W) {
 			cout << "{";
-			for (int j = 0; j <=i; j++)				
+			for (int j = 0; j <= i; j++)
 				if (include[j]) {
 					if (cnt > 0)
 						cout << ",";
@@ -45,16 +57,16 @@ void sum_of_subset(int i, int weight, int total, int *w, bool *include, int	W) {
 		}
 		else {
 			include[i + 1] = true;
-			sum_of_subset(i + 1, weight + w[i + 1], total - w[i + 1], w, include, W);
+			sum_of_subset(i + 1, weight + w[i + 1], total - w[i + 1]);
 			include[i + 1] = false;
-			sum_of_subset(i + 1, weight, total - w[i + 1], w, include, W);
+			sum_of_subset(i + 1, weight, total - w[i + 1]);
 
 		}
-			
+
 	}
 
 }
 
-bool promising(int i, int weight, int total, int *w, int W) {
+bool promising(int i, int weight, int total) {
 	return (weight + total >= W) && (weight == W || weight + w[i + 1] <= W);
 }
